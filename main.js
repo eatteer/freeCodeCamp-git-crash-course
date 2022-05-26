44b8dc176d4e97f7012a1f42795bfa4d49f995f3
eatteer <eatteer@gmail.com>
2022-05-25 22:29:24 -0500

    Modify name of functions

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const numbers = [1, 2, 3, 4, 5]
 
-const deleteNumberFromArray = (numberToDelete) => {
+const deleteNumber = (numberToDelete) => {
   return numbers.filter(number => number !== numberToDelete)
 }
 
-const addNumberToArray = (number) => {
+const addNumber = (number) => {
   return numbers.concat(number)
 }
