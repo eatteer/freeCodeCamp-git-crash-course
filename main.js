68fc53fa236a20a45a572223be71f547d8aaf126
eatteer <eatteer@gmail.com>
2022-05-25 22:24:01 -0500

    Implement adding numbers to array

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ const numbers = [1, 2, 3, 4, 5]
 const deleteNumberFromArray = (numberToDelete) => {
   return numbers.filter(number => number !== numberToDelete)
 }
+
+const addNumberToArray = (number) => {
+  return [...numbers, number]
+}
