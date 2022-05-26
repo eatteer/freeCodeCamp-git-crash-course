ffc619db7ab7117f6155cad5f03ac0b4fb4b99f9
eatteer <eatteer@gmail.com>
2022-05-25 22:11:14 -0500

    Implement number deletion from array

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 const numbers = [1, 2, 3, 4, 5]
+
+const deleteNumberFromArray = (numberToDelete, array) => {
+  return numbers.map(number => {
+    if (number === numberToDelete) {
+      return
+    }
+    return number
+  })
+}
